4c2f5d3dc02bba7c02fa163f440cd3f0a0cd2b6e
aljazsim <aljazsim@gmail.com>
2019-02-06 23:42:59 +1000

    updated angular example

--- examples/angular/src/app/app.model.ts
+++ examples/angular/src/app/app.model.ts
@@ -3,13 +3,10 @@ import { CannotBeNullOrWhitespace, MustBe, MustBeInteger, MustBeLongerThanOrEqua
 
 export class AppModel extends Validatable
 {
-    // #region Properties (6)
-
     @MustBe(x => x === true, "You must agree to terms and conditions.")
     public agreeTermsAndConditions = false;
 
     @MustBeValidDate()
-    // // @MustBeDateInThePast()
     public dateOfBirth = "";
 
     @MustMatch(/^(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])$/, "mustBeEmail")
@@ -31,8 +28,6 @@ export class AppModel extends Validatable
 
     @MustBeValidCreditCardNumber()
     public creditCardNumber = "";
-
-    // #endregion
 
     public validate(propertyName?: string): ValidationMessageCollection
     {
